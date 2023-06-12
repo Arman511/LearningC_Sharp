c01e692c79c9d8e5aae9c8a0bd9af7aafa170026
Arman <arman.shaikh.work@gmail.com>
2023-06-12 22:12:44 +0100

    used array

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] grades;
+        grades = new int[5];
+        Random rand = new Random();
+        for (int i = 0; i < grades.Length; i++)
+        {
+            grades[i] = rand.Next(1000);
+        }
+
+        foreach (int i in grades)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
