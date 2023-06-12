538fc06247ebfeb7ea63bd8a387401fb0b7c7340
Arman <arman.shaikh.work@gmail.com>
2023-06-12 23:38:27 +0100

    .

---
+++ 2dArrays/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[,,] matrix = {
+            {
+                {1,2,3},
+                {1,2,3},
+                {1,2,3}
+            },
+            {
+                {4,5,6},
+                {4,5,6},
+                {4,5,6}
+            },
+            {
+                {7,8,9},
+                {7,8,9},
+                {7,8,9}
+            },
+        };
+
+        Console.WriteLine("Last Value is {0}", matrix[2, 2, 2]);
+
+        int[,] twoD = new int[,] {
+            {1,2,3},
+            {4,5,6},
+            {8,9,0}
+        };
+        Console.WriteLine("Central Value is {0}", twoD[1, 1]);
+
+
+        string[,] stringArray = new String[5, 2]{
+            {"1", "2"}, {"3","4"},{"5","6"},{"7","8"},{"9","0"}
+        };
+
+        int dimension = stringArray.Rank;
+        
+        Tuple();
+
+
+
+    }
+
+    private static void Tuple()
+    {
+        var stuff = ("hello", 123, true);
+        Console.WriteLine(stuff.Item1);
+    }
+}
--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -7,12 +7,55 @@ class Program
         int[] grades;
         grades = new int[5];
         Random rand = new Random();
+        RandArrayAndDisplay(grades, rand);
+
+        Console.WriteLine();
+        InlineArrayInt();
+        Console.WriteLine();
+        grades = InlineArrayReturning();
+        Console.WriteLine();
+        InlineArrayString();
+
+    }
+
+    private static void RandArrayAndDisplay(int[] grades, Random rand)
+    {
         for (int i = 0; i < grades.Length; i++)
         {
             grades[i] = rand.Next(1000);
         }
 
         foreach (int i in grades)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    private static void InlineArrayInt()
+    {
+        int[] neoGrades = { 123, 123, 123, 234, 645 };
+        foreach (int i in neoGrades)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    private static int[] InlineArrayReturning()
+    {
+        int[] grades = new int[] { 123, 123, 123, 234, 645 };
+        foreach (int i in grades)
+        {
+            Console.WriteLine(i);
+        }
+
+        return grades;
+    }
+
+    private static void InlineArrayString()
+    {
+        String[] friends = { "Jeff", "Jermey", "James", "Jamil", "Jessica" };
+
+        foreach (string i in friends)
         {
             Console.WriteLine(i);
         }
